1ab514b737de371d814b82bf9b2f5450bb3d7b52
li.gen <li.gen@alibaba-inc.com>
2016-12-22 14:12:15 +0800

    init

---
+++ app/src/main/java/com/amap/naviquickstart/MainActivity.java
@@ -0,0 +1,218 @@
+package com.amap.naviquickstart;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import com.amap.api.location.AMapLocation;
+import com.amap.api.location.AMapLocationClient;
+import com.amap.api.location.AMapLocationClientOption;
+import com.amap.api.location.AMapLocationListener;
+import com.amap.api.maps.AMap;
+import com.amap.api.maps.SupportMapFragment;
+import com.amap.api.maps.model.BitmapDescriptorFactory;
+import com.amap.api.maps.model.Circle;
+import com.amap.api.maps.model.CircleOptions;
+import com.amap.api.maps.model.LatLng;
+import com.amap.api.maps.model.Marker;
+import com.amap.api.maps.model.MarkerOptions;
+import com.amap.api.navi.model.NaviLatLng;
+import com.amap.api.services.core.AMapException;
+import com.amap.api.services.core.LatLonPoint;
+import com.amap.api.services.core.PoiItem;
+import com.amap.api.services.poisearch.PoiResult;
+import com.amap.api.services.poisearch.PoiSearch;
+import com.amap.naviquickstart.overlay.PoiOverlay;
+import com.amap.naviquickstart.util.Utils;
+
+public class MainActivity extends AppCompatActivity implements AMapLocationListener, PoiSearch.OnPoiSearchListener, AMap.OnInfoWindowClickListener, AMap.OnMarkerClickListener, AMap.InfoWindowAdapter {
+    private AMap mMap;
+    private PoiSearch mPoiSearch;
+    private AMapLocationClient mLocationClient;
+    private AMapLocationClientOption mLocationOption;
+    private Marker mLocationMarker;
+    private Circle mLocationCircle;
+    private PoiOverlay mPoiOverlay;
+    private AMapLocation mCurrentLocation;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setUpMapIfNeeded();
+        initLocation();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        setUpMapIfNeeded();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        destroyLocation();
+    }
+
+    private void setUpMapIfNeeded() {
+        if (mMap == null) {
+            mMap = ((SupportMapFragment) getSupportFragmentManager()
+                    .findFragmentById(R.id.map)).getMap();
+            mMap.setOnMarkerClickListener(this);
+            mMap.setOnInfoWindowClickListener(this);
+            mMap.setInfoWindowAdapter(this);
+        }
+    }
+
+    /**
+     * 进行poi搜索
+     *
+     * @param lat
+     * @param lon
+     */
+    private void initPoiSearch(double lat, double lon) {
+        if (mPoiSearch == null) {
+            PoiSearch.Query poiQuery = new PoiSearch.Query("", "餐饮服务");
+            LatLonPoint centerPoint = new LatLonPoint(lat, lon);
+            PoiSearch.SearchBound searchBound = new PoiSearch.SearchBound(centerPoint, 5000);
+            mPoiSearch = new PoiSearch(this.getApplicationContext(), poiQuery);
+            mPoiSearch.setBound(searchBound);
+            mPoiSearch.setOnPoiSearchListener(this);
+            mPoiSearch.searchPOIAsyn();
+        }
+    }
+
+
+    private void destroyLocation() {
+        if (mLocationClient != null) {
+            mLocationClient.unRegisterLocationListener(this);
+            mLocationClient.onDestroy();
+        }
+    }
+
+    /**
+     * 初始化定位
+     */
+    private void initLocation() {
+        mLocationOption = new AMapLocationClientOption();
+        mLocationOption.setLocationMode(AMapLocationClientOption.AMapLocationMode.Hight_Accuracy);
+        mLocationOption.setOnceLocation(true);
+        mLocationClient = new AMapLocationClient(this.getApplicationContext());
+        mLocationClient.setLocationListener(this);
+        mLocationClient.startLocation();
+    }
+
+    @Override
+    public void onLocationChanged(AMapLocation aMapLocation) {
+        if (aMapLocation == null || aMapLocation.getErrorCode() != AMapLocation.LOCATION_SUCCESS) {
+            return;
+        }
+        mCurrentLocation = aMapLocation;
+        LatLng curLatLng = new LatLng(aMapLocation.getLatitude(), aMapLocation.getLongitude());
+        if (mLocationMarker == null) {
+            MarkerOptions markerOptions = new MarkerOptions();
+            markerOptions.position(curLatLng);
+            markerOptions.anchor(0.5f, 0.5f);
+            markerOptions.icon(BitmapDescriptorFactory.fromResource(R.mipmap.navi_map_gps_locked));
+            mLocationMarker = mMap.addMarker(markerOptions);
+        }
+        if (mLocationCircle == null) {
+            CircleOptions circleOptions = new CircleOptions();
+            circleOptions.center(curLatLng);
+            circleOptions.radius(aMapLocation.getAccuracy());
+            circleOptions.strokeWidth(2);
+            circleOptions.strokeColor(getResources().getColor(R.color.stroke));
+            circleOptions.fillColor(getResources().getColor(R.color.fill));
+            mLocationCircle = mMap.addCircle(circleOptions);
+        }
+        initPoiSearch(aMapLocation.getLatitude(), aMapLocation.getLongitude());
+    }
+
+    @Override
+    public void onPoiSearched(PoiResult poiResult, int i) {
+        if (i != AMapException.CODE_AMAP_SUCCESS || poiResult == null) {
+            return;
+        }
+        if (mPoiOverlay != null) {
+            mPoiOverlay.removeFromMap();
+        }
+        mPoiOverlay = new PoiOverlay(mMap, poiResult.getPois());
+        mPoiOverlay.addToMap();
+        mPoiOverlay.zoomToSpan();
+    }
+
+    @Override
+    public void onPoiItemSearched(PoiItem poiItem, int i) {
+
+    }
+
+    @Override
+    public void onInfoWindowClick(Marker marker) {
+
+    }
+
+    @Override
+    public boolean onMarkerClick(Marker marker) {
+        if (mLocationMarker == marker) {
+            return false;
+        }
+
+        return false;
+    }
+
+    /**
+     * 自定义marker点击弹窗内容
+     *
+     * @param marker
+     * @return
+     */
+    @Override
+    public View getInfoWindow(final Marker marker) {
+        View view = getLayoutInflater().inflate(R.layout.poikeywordsearch_uri,
+                null);
+        TextView title = (TextView) view.findViewById(R.id.title);
+        title.setText(marker.getTitle());
+
+        TextView snippet = (TextView) view.findViewById(R.id.snippet);
+        int index = mPoiOverlay.getPoiIndex(marker);
+        float distance = mPoiOverlay.getDistance(index);
+        String showDistance = Utils.getFriendlyDistance((int) distance);
+        snippet.setText("距当前位置" + showDistance);
+        ImageButton button = (ImageButton) view
+                .findViewById(R.id.start_amap_app);
+        // 调起高德地图app
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startAMapNavi(marker);
+            }
+        });
+        return view;
+    }
+
+    /**
+     * 点击一键导航按钮跳转到导航页面
+     *
+     * @param marker
+     */
+    private void startAMapNavi(Marker marker) {
+        if (mCurrentLocation == null) {
+            return;
+        }
+        Intent intent = new Intent(this, RouteNaviActivity.class);
+        intent.putExtra("gps", false);
+        intent.putExtra("start", new NaviLatLng(mCurrentLocation.getLatitude(), mCurrentLocation.getLongitude()));
+        intent.putExtra("end", new NaviLatLng(marker.getPosition().latitude, marker.getPosition().longitude));
+        startActivity(intent);
+    }
+
+    @Override
+    public View getInfoContents(Marker marker) {
+        return null;
+    }
+}
+
