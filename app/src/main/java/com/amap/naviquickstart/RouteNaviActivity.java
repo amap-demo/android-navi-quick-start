06e8d8ac1a26b91c98e97eb62ae79b512592a85b
my94493 <my94493@autonavi.com>
2017-08-22 10:24:59 +0800

    增加新包的回调接口

--- app/src/main/java/com/amap/naviquickstart/RouteNaviActivity.java
+++ app/src/main/java/com/amap/naviquickstart/RouteNaviActivity.java
@@ -152,6 +152,11 @@ public class RouteNaviActivity extends Activity implements AMapNaviListener, AMa
     }
 
     @Override
+    public void onGetNavigationText(String s) {
+
+    }
+
+    @Override
     public void onEndEmulatorNavi() {
     }
 
